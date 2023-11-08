17d6be4de1ba9a47b8d529e2d36c47ab461f4a54
dhumalkomal <107318301+dhumalkomal@users.noreply.github.com>
2023-11-08 16:31:49 +0530

    update

--- config/mongoose.js
+++ config/mongoose.js
@@ -5,8 +5,9 @@ const mongoose = require('mongoose');
 // mongoDB url stored in env variable
 // const  MONGODB_URL  = 'mongodb://127.0.0.1:27017/ERS_db';
 
-const  MONGODB_URL  = 'mongodb+srv://komaldhumal:EmployeeSystem@komalcluster.lythlnz.mongodb.net/?retryWrites=true&w=majority';
+// const  MONGODB_URL  = 'mongodb+srv://komaldhumal:EmployeeSystem@komalcluster.lythlnz.mongodb.net/?retryWrites=true&w=majority';
 
+const  MONGODB_URL  = 'mongodb+srv://habit:habit123@cluster0.krqzlcy.mongodb.net/?retryWrites=true&w=majoritymongodb+srv://komaldhumal:EmployeeSystem@komalcluster.lythlnz.mongodb.net/?retryWrites=true&w=majority';
 
 
 // connect to database
